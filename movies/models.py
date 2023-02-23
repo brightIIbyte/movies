d99da5109f1e982a744f9418d055316fa0bc2aba
Anuar <126041946+brightIIbyte@users.noreply.github.com>
2023-02-23 09:53:16 +0600

    test

--- movies/models.py
+++ movies/models.py
@@ -104,3 +104,6 @@ class Rating(models.Model):
     ip = models.CharField("IP адрес", max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
     movie = models.ForeignKey(Movie, on_delete=models.CharField)
+
+
+# test
