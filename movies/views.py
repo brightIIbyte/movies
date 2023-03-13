0aa33d56efeb4dcb038311231f3a7e3550a7c2a7
Anuar <126041946+brightIIbyte@users.noreply.github.com>
2023-03-13 22:02:43 +0600

    bye

--- movies/views.py
+++ movies/views.py
@@ -9,6 +9,7 @@ class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, "movies/movie_list.html", {"movie_list": movies})
+
 class MovieDataView(View):
     """Полное описание фильма"""
     def get(self, request, pk):
